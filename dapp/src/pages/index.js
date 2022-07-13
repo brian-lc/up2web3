456c1904cb7f883ed71d674aa20e6d9650b4f41e
Brian Chamberlain <brian.chamberlain@mesh.xyz>
2022-07-13 10:17:51 -0400

    Basic gatsby page with mantine installed

---
+++ dapp/src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { Button } from '@mantine/core';
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <Button>Connect wallet</Button>
+    </main>
+  )
+}
+
+export default IndexPage
